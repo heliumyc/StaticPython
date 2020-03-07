cadfabbaf3683a03d5986b3a019c7135549007c4
heliumyc <ericyc4@gmail.com>
2020-03-07 12:57:23 -0500

    adjust indentation

--- src/main/scala/parser/ASTNode.scala
+++ src/main/scala/parser/ASTNode.scala
@@ -4,11 +4,11 @@ import lexer.Token
 
 import scala.collection.mutable
 
-class ASTNode (val token:Token, val parent: Option[ASTNode] = None) {
-  val children: mutable.ArrayBuffer[ASTNode] = mutable.ArrayBuffer[ASTNode]()
+class ASTNode(val token: Token, val parent: Option[ASTNode] = None) {
+    val children: mutable.ArrayBuffer[ASTNode] = mutable.ArrayBuffer[ASTNode]()
 
-  def addChild(node: ASTNode): Unit = {
-    children.addOne(node)
-  }
+    def addChild(node: ASTNode): Unit = {
+        children.addOne(node)
+    }
 
 }
