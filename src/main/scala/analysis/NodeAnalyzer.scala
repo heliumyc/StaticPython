7feb26ffe86d5e94a117b38de5335666044e9087
heliumyc <ericyc4@gmail.com>
2020-04-07 18:28:31 -0400

    init node analyzer with visitor pattern

---
+++ src/main/scala/analysis/NodeAnalyzer.scala
@@ -0,0 +1,40 @@
+package analysis
+
+import astnodes.{BreakStmt, ContinueStmt, DelStmt, ErrorStmt, ForStmt, IdType, IfStmt, ImportStmt, PassStmt, Program, ReturnStmt, TypedVar, WhileStmt}
+import astnodes.declarations._
+import astnodes.expressions._
+import astnodes.literals.{BoolLiteral, FloatLiteral, IntegerLiteral, NoneLiteral, StringLiteral}
+
+trait NodeAnalyzer[T] {
+    def analyze(node: ClassDef): T
+    def analyze(node: FuncDef): T
+    def analyze(node: VarDef): T
+    def analyze(node: AssignExpr): T
+    def analyze(node: BinaryExpr): T
+    def analyze(node: CallExpr): T
+    def analyze(node: Identifier): T
+    def analyze(node: IfExpr): T
+    def analyze(node: IndexExpr): T
+    def analyze(node: ListExpr): T
+    def analyze(node: MemberExpr): T
+    def analyze(node: TupleExpr): T
+    def analyze(node: UnaryExpr): T
+    def analyze(node: BoolLiteral): T
+    def analyze(node: FloatLiteral): T
+    def analyze(node: IntegerLiteral): T
+    def analyze(node: NoneLiteral): T
+    def analyze(node: StringLiteral): T
+    def analyze(node: BreakStmt): T
+    def analyze(node: ContinueStmt): T
+    def analyze(node: DelStmt): T
+    def analyze(node: ErrorStmt): T
+    def analyze(node: ForStmt): T
+    def analyze(node: IdType): T
+    def analyze(node: IfStmt): T
+    def analyze(node: ImportStmt): T
+    def analyze(node: PassStmt): T
+    def analyze(node: Program): T
+    def analyze(node: ReturnStmt): T
+    def analyze(node: TypedVar): T
+    def analyze(node: WhileStmt): T
+}
--- src/main/scala/astnodes/PyAst.scala
+++ src/main/scala/astnodes/PyAst.scala
@@ -1,5 +1,11 @@
 package astnodes
 
+import java.lang.reflect.Member
+
+import analysis.NodeAnalyzer
+import astnodes.declarations.{ClassDef, FuncDef, VarDef}
+import astnodes.expressions.{AssignExpr, BinaryExpr, CallExpr, Identifier, IfExpr, IndexExpr, ListExpr, MemberExpr, TupleExpr, UnaryExpr}
+import astnodes.literals.{BoolLiteral, FloatLiteral, IntegerLiteral, NoneLiteral, StringLiteral}
 import lexer.{NoPosition, PyPosition}
 
 trait PyAst {
@@ -7,5 +13,41 @@ trait PyAst {
     def setPos(position: PyPosition): this.type = {
         this.pos = position
         this
+    }
+
+    def dispatch[T](nodeAnalyzer: NodeAnalyzer[T]): T = {
+        this match {
+            case t:ClassDef => nodeAnalyzer.analyze(t)
+            case t:FuncDef => nodeAnalyzer.analyze(t)
+            case t:VarDef => nodeAnalyzer.analyze(t)
+            case t:AssignExpr => nodeAnalyzer.analyze(t)
+            case t:BinaryExpr => nodeAnalyzer.analyze(t)
+            case t:CallExpr => nodeAnalyzer.analyze(t)
+            case t:Identifier => nodeAnalyzer.analyze(t)
+            case t:IfExpr => nodeAnalyzer.analyze(t)
+            case t:IndexExpr => nodeAnalyzer.analyze(t)
+            case t:ListExpr => nodeAnalyzer.analyze(t)
+            case t:MemberExpr => nodeAnalyzer.analyze(t)
+            case t:TupleExpr => nodeAnalyzer.analyze(t)
+            case t:UnaryExpr => nodeAnalyzer.analyze(t)
+            case t:BoolLiteral => nodeAnalyzer.analyze(t)
+            case t:FloatLiteral => nodeAnalyzer.analyze(t)
+            case t:IntegerLiteral => nodeAnalyzer.analyze(t)
+            case t:NoneLiteral => nodeAnalyzer.analyze(t)
+            case t:StringLiteral => nodeAnalyzer.analyze(t)
+            case t:BreakStmt => nodeAnalyzer.analyze(t)
+            case t:ContinueStmt => nodeAnalyzer.analyze(t)
+            case t:DelStmt => nodeAnalyzer.analyze(t)
+            case t:ErrorStmt => nodeAnalyzer.analyze(t)
+            case t:ForStmt => nodeAnalyzer.analyze(t)
+            case t:IdType => nodeAnalyzer.analyze(t)
+            case t:IfStmt => nodeAnalyzer.analyze(t)
+            case t:ImportStmt => nodeAnalyzer.analyze(t)
+            case t:PassStmt => nodeAnalyzer.analyze(t)
+            case t:Program => nodeAnalyzer.analyze(t)
+            case t:ReturnStmt => nodeAnalyzer.analyze(t)
+            case t:TypedVar => nodeAnalyzer.analyze(t)
+            case t:WhileStmt => nodeAnalyzer.analyze(t)
+        }
     }
 }
